f314e7f26b145e21b3d7605ae736cde6cbd954bd
Chingly <kavisara.noya@mail.kmutt.ac.th>
2021-02-04 20:18:13 +0700

    [ADD] CSC234

---
+++ CSC234/assignment2/lib/main.dart
@@ -0,0 +1,67 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('Kavisara 62130500254'),
+            ),
+            body: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Icon(Icons.account_circle, size: 50),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          'Kavisara Srisuwatcharee',
+                          style: Theme.of(context).textTheme.headline,
+                        ),
+                        Text(
+                          '62130500254',
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+                SizedBox(height: 8),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      '114 Phetkasem',
+                    ),
+                    Text(
+                      '0970892143',
+                    ),
+                  ],
+                ),
+                SizedBox(height: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Icon(Icons.accessibility),
+                    Icon(Icons.timer),
+                    Icon(Icons.phone_android),
+                    Icon(Icons.phone_iphone),
+                  ],
+                ),
+              ],
+            )));
+  }
+}
