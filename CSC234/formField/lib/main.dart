ff5d45a9b1ced4d700929ebd2ed00b281fe5619e
Chingly <kavisara.noya@mail.kmutt.ac.th>
2021-02-19 16:30:22 +0700

    [ADD] Assignment CSC234

--- CSC234/formField/lib/main.dart
+++ CSC234/formField/lib/main.dart
@@ -10,9 +10,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'MyForm',
       theme: ThemeData(
-          primaryColor: Colors.lightGreen[400],
-          textTheme:
-              Theme.of(context).textTheme.apply(bodyColor: Colors.amber)),
+        primaryColor: Colors.lightGreen[400],
+        // textTheme:
+        //     Theme.of(context).textTheme.apply(bodyColor: Colors.amber)),
+      ),
       home: Scaffold(
           appBar: AppBar(
             title: Text('Form Validation Demo'),
@@ -28,28 +29,102 @@ class MyForm extends StatefulWidget {
 }
 
 class _MyFormState extends State<MyForm> {
+  final _nameFocusNode = FocusNode();
+  final _idFocusNode = FocusNode();
+  final _formKey = GlobalKey<FormState>();
+  var name = "";
+  var id = "";
+
+  void _saveForm() {
+    if (_formKey.currentState.validate()) {
+      _formKey.currentState.save();
+      FocusScope.of(context).requestFocus(this._nameFocusNode);
+      showDialog(
+          context: context,
+          builder: (ctx) => new AlertDialog(
+                title: Text('Your information'),
+                content: Container(
+                  height: 100,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(this.name),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text(this.id)
+                    ],
+                  ),
+                ),
+                actions: <Widget>[
+                  FlatButton(
+                    child: Text(
+                      'Close me!',
+                      style: TextStyle(color: Colors.amber[500]),
+                    ),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  )
+                ],
+              ));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(30),
-      child: Form(
-        child: Column(
-          children: <Widget>[
-            TextFormField(
-              decoration: InputDecoration(labelText: 'Name'),
-            ),
-            TextFormField(
-              decoration: InputDecoration(labelText: 'Student ID'),
-            ),
-            Container(
-              padding: EdgeInsets.all(50),
-              child: Column(
-                children: <Widget>[
-                  RaisedButton(onPressed: null, child: Text('submit'))
-                ],
+    return SingleChildScrollView(
+      child: Container(
+        padding: EdgeInsets.all(30),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter your name';
+                  }
+                  return null;
+                },
+                decoration: InputDecoration(labelText: 'Name'),
+                textInputAction: TextInputAction.next,
+                onSaved: (value) {
+                  this.name = value;
+                },
+                focusNode: this._nameFocusNode,
               ),
-            )
-          ],
+              TextFormField(
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter student ID';
+                  }
+                  return null;
+                },
+                decoration: InputDecoration(labelText: 'Student ID'),
+                keyboardType: TextInputType.number,
+                onSaved: (value) {
+                  this.id = value;
+                },
+                focusNode: this._idFocusNode,
+              ),
+              Container(
+                padding: EdgeInsets.all(50),
+                child: Column(
+                  children: <Widget>[
+                    RaisedButton(
+                      color: Colors.amber,
+                      onPressed: _saveForm,
+                      child: Text('submit'),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
