d57aba280f69e72bf094c63324c98455179d6b69
Chingly <kavisara.noya@mail.kmutt.ac.th>
2021-02-26 17:20:58 +0700

    [ADD] Nevigator flutter

---
+++ CSC234/NevigationHW/android/app/src/main/kotlin/com/example/NevigationHW/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.NevigationHW
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ CSC234/NevigationHW/lib/main.dart
@@ -0,0 +1,182 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'HomePage',
+      theme: ThemeData(
+        primaryColor: Colors.lightGreen[400],
+        // textTheme:
+        //     Theme.of(context).textTheme.apply(bodyColor: Colors.amber)),
+      ),
+      home: Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            title: Text(
+              'Mutipages Navigation',
+            ),
+          ),
+          body: HomePage()),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final textController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Container(
+        padding: EdgeInsets.all(30),
+        child: Form(
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                controller: textController,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20))),
+              ),
+              Container(
+                padding: EdgeInsets.all(50),
+                child: Column(
+                  children: <Widget>[
+                    RaisedButton(
+                      color: Colors.amber,
+                      onPressed: () {
+                        var value = textController.text;
+                        if (value.isEmpty) {
+                          if (value == 'text01') {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => Page01()));
+                            return;
+                          }
+                          if (value == 'text02') {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => Page02()));
+                            return;
+                          }
+
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => UnknownPage()));
+                        }
+                      },
+                      child: Text('submit'),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Page01 extends StatelessWidget {
+  const Page01({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.all(20),
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Saweedee ka Jarn Toei'),
+            RaisedButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text('back'),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                )),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Page02 extends StatelessWidget {
+  const Page02({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.all(20),
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Saweedee ka Jarn Toei'),
+            RaisedButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text('back'),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                )),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class UnknownPage extends StatelessWidget {
+  const UnknownPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.all(20),
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Mai mee information'),
+            RaisedButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text('back'),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                )),
+          ],
+        ),
+      ),
+    );
+  }
+}
